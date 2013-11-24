71399d628769b514933c5e6d2c8d0c58b917f09e
Ashley Wright <AshleyMarieEvans@gmail.com>
2013-11-24 14:58:38 -0600

    Register Form

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -4,17 +4,50 @@ $(document).ready(initialize);
 
 function initialize(){
   $(document).foundation();
+  // Go to register form
+  $('#register-link').on('click', clickRegisterLink);
+  $('#register-form button').on('click', clickRegister);
 }
 
 // =============== Events ================= //
 // ======================================== //
 // ======================================== //
 
+function clickRegisterLink(e){
+  $('#sign-in-form').addClass('hidden');
+  $('#register-form').removeClass('hidden');
+  $('#register-username').focus();
+  e.preventDefault();
+}
+
+function clickRegister(e){
+  var username = $('#register-username').val();
+  var password = $('#register-password1').val();
+  var password2 = $('#register-password2').val();
+
+  $('#register-error').addClass('hidden');
+  $('#register-error').text('');
+
+  if(!username){
+    updateRegisterError('Username is required')
+  } else if(!password || !password2) {
+    updateRegisterError('Password is required')
+  } else if(password !== password2){
+    updateRegisterError('Passwords did not match')
+  } else {
+
+  }
+
+  e.preventDefault();
+}
 
 
 // ========================================= //
 // ================ HTML =================== //
 // ========================================= //
 
-
+function updateRegisterError(message){
+  $('#register-error').removeClass('hidden');
+  $('#register-error').text(message);
+}
 
