b06dd31aa84ace6745d1ef6e3a6a787095d07800
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-03-08 18:38:28 -0600

    WIP: Searched notes, user id not mathing notes

--- routes/notes.js
+++ routes/notes.js
@@ -15,20 +15,31 @@ exports.search = function(req,res){
   req.session.searchTerm = search;
   req.session.searchResults = [];
   req.session.sortedSearchResults = [];
-  // console.log('-----');
-  // console.log(res.locals.user);
+  console.log('-----');
+  console.log(res.locals.user._id);
 
   Note.textSearch(search, function(err, output){
     if(output){
       req.session.searchResults = output.results;
+      console.log(output);
       res.send(output.results);
     }
   });
 
   // Note.textSearch(search, function(err, output){
+  //   console.log("Text Search");
+  //   console.log("res");
+  //   console.log(res.locals.user._id);
+  //   // console.log(output)
+  //   // console.log(output.results)
   //   if(output){
   //     for(var i = 0; i < output.results.length; i++){
-  //       if(output.results[i].obj.user === req.locals.user){
+  //       console.log("Test for");
+  //       console.log("res");
+  //       console.log(res.locals.user._id);
+  //       console.log(output.results[i].obj.user);
+  //       if(output.results[i].obj.user == res.locals.user._id){
+  //         console.log("In loop");
   //         console.log(output.results[i]);
   //       }
   //     }
@@ -40,6 +51,7 @@ exports.search = function(req,res){
 
   // Note.find({user: res.locals.user}, function(err, notes){
   //   for(var i = 0; i < notes.length; i++){
+  //     console.log("find users notes")
   //     console.log(notes[i]);
   //   }
   // });
