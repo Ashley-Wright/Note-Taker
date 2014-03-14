4bd724ac61b7c6605828e67bc3074950f41cb171
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-03-14 14:24:47 -0500

    Fixed filtering user id in search notes

--- routes/notes.js
+++ routes/notes.js
@@ -15,45 +15,16 @@ exports.search = function(req,res){
   req.session.searchTerm = search;
   req.session.searchResults = [];
   req.session.sortedSearchResults = [];
-  console.log('-----');
-  console.log(res.locals.user._id);
 
   Note.textSearch(search, function(err, output){
+
     if(output){
-      req.session.searchResults = output.results;
-      console.log(output);
-      res.send(output.results);
+      for(var i = 0; i < output.results.length; i++){
+        if(String(output.results[i].obj.user) == String(res.locals.user._id)){
+          req.session.searchResults.push(output.results[i])
+        }
+      }
+      res.send(req.session.searchResults);
     }
   });
-
-  // Note.textSearch(search, function(err, output){
-  //   console.log("Text Search");
-  //   console.log("res");
-  //   console.log(res.locals.user._id);
-  //   // console.log(output)
-  //   // console.log(output.results)
-  //   if(output){
-  //     for(var i = 0; i < output.results.length; i++){
-  //       console.log("Test for");
-  //       console.log("res");
-  //       console.log(res.locals.user._id);
-  //       console.log(output.results[i].obj.user);
-  //       if(output.results[i].obj.user == res.locals.user._id){
-  //         console.log("In loop");
-  //         console.log(output.results[i]);
-  //       }
-  //     }
-  //     // console.log(output.results);
-  //     res.send({});
-  //   }
-  // });
-
-
-  // Note.find({user: res.locals.user}, function(err, notes){
-  //   for(var i = 0; i < notes.length; i++){
-  //     console.log("find users notes")
-  //     console.log(notes[i]);
-  //   }
-  // });
-
 }
